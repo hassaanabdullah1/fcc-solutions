6cf605218af0accbf94a8d5324f237654448740e
Hassaan Abdullah <226998312+hassaanabdullah1@users.noreply.github.com>
2025-09-20 19:59:41 +0500

    Update and rename Sep_3_2025 [pangram].py to Sep_03_2025 [pangram].py

--- 2025/September/Sep_03_2025 [pangram].py
+++ 2025/September/Sep_03_2025 [pangram].py
@@ -1,5 +1,3 @@
-import time
-start_time = time.perf_counter()
 '''
 ---Pangram---
 Given a word or sentence and a string of lowercase letters, determine if the word or sentence uses all the letters from the given set at least once and no other letters.
@@ -8,8 +6,6 @@ Given a word or sentence and a string of lowercase letters, determine if the wor
 '''
 def is_pangram(sentence, letters):
     new_sent = []
-    #You can separate the characters in a string in Python by using the list() function, which converts the string into a list of its individual characters. For example, list("hello") will give you ['h', 'e', 'l', 'l', 'o']
-    #dict.fromkeys() method creates a dictionary with keys from the list, automatically removing duplicates because dictionary keys are unique. The order is preserved where dictionaries maintain insertion order.
     found = False
     sentence = list(dict.fromkeys(sentence))
     new_let = list(x.lower() for x in letters) #could be simplified to [x.lower() for x in letters] or even done earlier when processing the input.
@@ -30,4 +26,5 @@ is_pangram("Hello World!", "helowrd") #should return True
 is_pangram("Hello World!", "heliowrd") #should return False
 is_pangram("freeCodeCamp", "frcdmp") #should return False
 is_pangram("The quick brown fox jumps over the lazy dog.", "abcdefghijklmnopqrstuvwxyz") #should return True
+
 
